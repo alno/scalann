114c22af69e2fa6af0b721a02eb0be52ba0e1d5b
Alexey Noskov <alexey.noskov@gmail.com>
2013-01-10 02:19:52 +0400

    New bench

--- src/test/scala/org/scalann/LearningBench.scala
+++ src/test/scala/org/scalann/LearningBench.scala
@@ -21,17 +21,28 @@ class LearningBench extends SimpleBenchmark {
   val example = examples.head
 
   val nn = new FeedForwardNetwork(List(new LogisticLayer(w * h, 200), new SoftmaxLayer(200, 10)))
+  val l = new LogisticLayer(w * h, 10)
 
   override def setUp {
     examples.size
   }
 
-  def timeForward(reps: Int) {
+  def timeLayerForward(reps: Int) {
+    for (i <- 1 to reps)
+      l.forward(example._1)
+  }
+
+  def timeLayerForwardBackward(reps: Int) {
+    for (i <- 1 to reps)
+      l.forward(example._1)._2.backward(example._2)
+  }
+
+  def timeNetworkForward(reps: Int) {
     for (i <- 1 to reps)
       nn.forward(example._1)
   }
 
-  def timeForwardBackward(reps: Int) {
+  def timeNetworkForwardBackward(reps: Int) {
     for (i <- 1 to reps)
       nn.forward(example._1)._2.backward(example._2)
   }
