7caf5bbd95e0076ce0f2a101602f60ed1a824e8c
Alexey Noskov <alexey.noskov@gmail.com>
2013-01-13 22:35:28 +0400

    Optimizing rbm gradient

--- src/main/scala/org/scalann/Rbm.scala
+++ src/main/scala/org/scalann/Rbm.scala
@@ -3,59 +3,62 @@ package org.scalann
 import breeze.linalg._
 import breeze.numerics._
 import Utils._
+import org.netlib.blas.Dgemm
 
 class Rbm(val inputSize: Int, val outputSize: Int) extends Parametrized {
   val paramSize = outputSize * inputSize
 
   val params = DenseVector.fill(paramSize) { math.random * 2 - 1 }
   val weights: DenseMatrix[Double] = new DenseMatrix(outputSize, inputSize, params.data)
- 
+
   private val weightsT: DenseMatrix[Double] = weights.t
 
-  private def visibleStateToHiddenProbs(visible: DenseVector[Double]): DenseVector[Double] = {
-    val result = weights * visible
-    sigmoid.inPlace(result)
-    result
-  }
-
-  private def hiddenStateToVisibleProbs(hidden: DenseVector[Double]): DenseVector[Double] = {
-    val result = weightsT * hidden
-    sigmoid.inPlace(result)
-    result
-  }
-  
   def updateParams(gradient: DenseVector[Double]) =
     params += gradient
 
   def assignParams(newParams: DenseVector[Double]) =
     params := newParams
-  
-  def gradient(visibleProb0: DenseVector[Double]): DenseVector[Double] = {
-    val res = DenseVector.zeros[Double](paramSize)
-    
-    val visibleData0 = sample(visibleProb0)
 
-    val hiddenProb0 = visibleStateToHiddenProbs(visibleData0)
-    val hiddenData0 = sample(hiddenProb0)
- 
-    val visibleProb1 = hiddenStateToVisibleProbs(hiddenData0)
-    val visibleData1 = sample(visibleProb1)
-       
-    val hiddenProb1 = visibleStateToHiddenProbs(visibleData1)
-    
-    new DenseMatrix(outputSize, inputSize, res.data) := hiddenData0 * visibleData0.t - hiddenProb1 * visibleData1.t
+  def gradientAdd(example: DenseVector[Double])(paramGradAcc: DenseVector[Double], factor: Double) = {
+    val visibleData0 = sample(example)
+
+    val hiddenData0 = weights * visibleData0
+    sigmoid.inPlace(hiddenData0)
+    sample.inPlace(hiddenData0)
+
+    val visibleData1 = weightsT * hiddenData0
+    sigmoid.inPlace(visibleData1)
+    sample.inPlace(visibleData1)
+
+    val hiddenProb1 = weights * visibleData1
+    sigmoid.inPlace(hiddenProb1)
+
+    // paramGradAcc += hiddenData0 * visibleData0.t
+    Dgemm.dgemm("n", "t", outputSize, inputSize, 1,
+      factor, hiddenData0.data, hiddenData0.offset, outputSize,
+      visibleData0.data, visibleData0.offset, inputSize,
+      1.0, paramGradAcc.data, paramGradAcc.offset, outputSize)
+
+    // paramGradAcc -= hiddenProb1 * visibleData1.t
+    Dgemm.dgemm("n", "t", outputSize, inputSize, 1,
+      -factor, hiddenProb1.data, hiddenProb1.offset, outputSize,
+      visibleData1.data, visibleData1.offset, inputSize,
+      1.0, paramGradAcc.data, paramGradAcc.offset, outputSize)
+  }
+
+  def gradient(example: DenseVector[Double]): DenseVector[Double] = {
+    val res = DenseVector.zeros[Double](paramSize)
+    gradientAdd(example)(res, 1.0)
     res
   }
-  
+
+  def gradientAdd(examples: Traversable[DenseVector[Double]])(paramGradAcc: DenseVector[Double], factor: Double): Unit =
+    examples.foreach { gradientAdd(_)(paramGradAcc, factor) }
+
   def gradient(examples: Traversable[DenseVector[Double]]): DenseVector[Double] = {
-    val grad = gradient(examples.head)
-    
-    examples.tail.foreach { ex =>
-      grad += gradient(ex)
-    }
-    
-    grad *= 1.0 / examples.size
-    grad
+    val res = DenseVector.zeros[Double](paramSize)
+    gradientAdd(examples)(res, 1.0 / examples.size)
+    res
   }
-  
+
 }
