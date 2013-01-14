707251d6ac9559cd3c0ea047d0a577b6855475b2
Alexey Noskov <alexey.noskov@gmail.com>
2013-01-15 01:25:36 +0400

    Util functions

--- src/main/scala/org/scalann/Utils.scala
+++ src/main/scala/org/scalann/Utils.scala
@@ -7,7 +7,20 @@ import scala.math._
 object Utils {
 
   def sample = UFunc { (x: Double) =>
-    if (x > random) 1.0 else 0.0
+    if (x > fastRandomDouble) 1.0 else 0.0
+  }
+
+  def zero = UFunc { (x: Any) => 0.0 }
+
+  private var randomState: Long = System.nanoTime
+
+  def fastRandomDouble = (fastRandomLong & 0xFFFF) / 65536.0
+
+  def fastRandomLong: Long = {
+    randomState ^= (randomState << 21)
+    randomState ^= (randomState >>> 35)
+    randomState ^= (randomState << 4)
+    randomState
   }
 
 }
