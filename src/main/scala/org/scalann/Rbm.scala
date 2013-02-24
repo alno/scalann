7c3afc562b10176bc9a5c9ff6d07651228ae6ef4
Alexey Noskov <alexey.noskov@gmail.com>
2013-02-24 19:26:15 +0400

    Rbm gradient tuning

--- src/main/scala/org/scalann/Rbm.scala
+++ src/main/scala/org/scalann/Rbm.scala
@@ -12,6 +12,8 @@ class Rbm(val inputSize: Int, val outputSize: Int) extends Optimizable[DenseVect
   val params = DenseVector.fill(paramSize) { math.random * 2 - 1 }
   val weights: DenseMatrix[Double] = new DenseMatrix(outputSize, inputSize, params.data)
 
+  var cdLevel = 1
+
   private val weightsT: DenseMatrix[Double] = weights.t
 
   def updateParams(gradient: DenseVector[Double]) =
@@ -20,6 +22,10 @@ class Rbm(val inputSize: Int, val outputSize: Int) extends Optimizable[DenseVect
   def assignParams(newParams: DenseVector[Double]) =
     params := newParams
 
+  /**
+   * Calculating gradient with CDn algorithm
+   * Tuning according to: http://www.cs.toronto.edu/~hinton/absps/guideTR.pdf
+   */
   def gradientAdd(example: DenseVector[Double])(paramGradAcc: DenseVector[Double], factor: Double) {
     if (paramGradAcc == null)
       return
@@ -27,8 +33,6 @@ class Rbm(val inputSize: Int, val outputSize: Int) extends Optimizable[DenseVect
     val visible = example.copy
     val hidden = DenseVector.zeros[Double](outputSize)
 
-    sample.inPlace(visible)
-
     // hidden = weights * visible
     Dgemv.dgemv("n", outputSize, inputSize,
       1.0, weights.data, weights.offset, weights.majorStride,
@@ -36,32 +40,37 @@ class Rbm(val inputSize: Int, val outputSize: Int) extends Optimizable[DenseVect
       0.0, hidden.data, hidden.offset, 1)
 
     sigmoid.inPlace(hidden)
-    sample.inPlace(hidden)
 
+    // Updating gradient with positive statistics
     // paramGradAcc += hidden * visible.t * factor
     Dgemm.dgemm("n", "t", outputSize, inputSize, 1,
       factor, hidden.data, hidden.offset, outputSize,
       visible.data, visible.offset, inputSize,
       1.0, paramGradAcc.data, paramGradAcc.offset, outputSize)
 
-    // visible = weights.t * hidden
-    Dgemv.dgemv("t", outputSize, inputSize,
-      1.0, weights.data, weights.offset, weights.majorStride,
-      hidden.data, hidden.offset, 1,
-      0.0, visible.data, visible.offset, 1)
+    // Iterating cdLevel times to generate reconstructions
+    for (i <- 0 to cdLevel) {
+      // Sample hidden state to create information bottleneck
+      sample.inPlace(hidden)
 
-    sigmoid.inPlace(visible)
-    sample.inPlace(visible)
+      // visible = weights.t * hidden
+      Dgemv.dgemv("t", outputSize, inputSize,
+        1.0, weights.data, weights.offset, weights.majorStride,
+        hidden.data, hidden.offset, 1,
+        0.0, visible.data, visible.offset, 1)
 
-    // hidden = weights * visible
-    Dgemv.dgemv("n", outputSize, inputSize,
-      1.0, weights.data, weights.offset, weights.majorStride,
-      visible.data, visible.offset, 1,
-      0.0, hidden.data, hidden.offset, 1)
+      sigmoid.inPlace(visible)
 
-    sigmoid.inPlace(hidden)
-    sample.inPlace(hidden)
+      // hidden = weights * visible
+      Dgemv.dgemv("n", outputSize, inputSize,
+        1.0, weights.data, weights.offset, weights.majorStride,
+        visible.data, visible.offset, 1,
+        0.0, hidden.data, hidden.offset, 1)
 
+      sigmoid.inPlace(hidden)
+    }
+
+    // Updating gradient with negative statistics
     // paramGradAcc -= hiddenProb1 * visibleData1.t * factor
     Dgemm.dgemm("n", "t", outputSize, inputSize, 1,
       -factor, hidden.data, hidden.offset, outputSize,
